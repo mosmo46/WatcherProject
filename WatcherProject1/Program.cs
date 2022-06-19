2be30649e6bb08b9241ffc0fb39666d6a69953f8
Moshe Yaso <yasomoshe@gmail.com>
2022-06-19 10:18:14 +0300

    commit

--- WatcherProject1/Program.cs
+++ WatcherProject1/Program.cs
@@ -13,6 +13,11 @@ namespace WatcherProject1
     class Program
 
     {
+        static string owner = "mosmo46";
+        static string name = "gitignore";
+
+        //static InMemoryCredentialStore credentials = new InMemoryCredentialStore(new Credentials("your-token-here"));
+      //  static ObservableGitHubClient client = new ObservableGitHubClient(new ProductHeaderValue("ophion"), credentials);
 
         static void Main(string[] args)
 
@@ -54,13 +59,28 @@ namespace WatcherProject1
             Console.ReadLine();
             
         }
+
+
+        private static void ComparisonBetween()
+        {
+
+            var ghe = new Uri("https://github.com/mosmo46/DemoApp");
+
+            var client1 = new GitHubClient(new ProductHeaderValue("DemoApp"), ghe);
+
+
+            Console.WriteLine($"client1=>>{client1}");
+
+
+
+        }
         private static async void uplodaToGithub(string path)
         {
             var ghClient = new GitHubClient(new ProductHeaderValue("DemoApp"));
 
-            ghClient.Credentials = new Credentials("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");
+            ghClient.Credentials = new Credentials("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");
 
-            var owner = "mosmo46";
+           // var owner = "mosmo46";
             var repo = "DemoApp";
             var master = "master";
             try
@@ -68,7 +88,7 @@ namespace WatcherProject1
                 var fileDetails = await ghClient.Repository.Content.GetAllContentsByRef(owner, repo,
                                         path, master);
                 var updateResult = await ghClient.Repository.Content.UpdateFile(owner, repo, path,
-                                         new UpdateFileRequest("My updated file", "SucceededSucceededSucceeded", fileDetails.First().Sha));
+                                         new UpdateFileRequest("My updated file", "Succeed", fileDetails.First().Sha));
             }
             catch (Octokit.NotFoundException)
             {
@@ -76,6 +96,65 @@ namespace WatcherProject1
                 await ghClient.Repository.Content.CreateFile(owner, repo, path, new CreateFileRequest("API File cs creation", "Hello Universe! " + DateTime.UtcNow, master));
             }
         }
+
+
+     
+        private static async Task CanGetFilesInCommit()
+        {
+
+
+
+
+            //var repo = "DemoApp";
+
+            //var client = new GitHubClient(new ProductHeaderValue("DemoApp"));
+
+            //var repository = await client.Repository.Commit.GetAll(owner, repo);
+
+            //var commitsFiltered = repository.Select(async (_) =>
+            //{
+            //    return await client.Repository.Commit.Compare(owner, repo, _.Sha);
+            //}).ToList();
+
+            //var commits = await Task.WhenAll(commitsFiltered);
+
+            //foreach (var item in commits)
+            //{
+            //    Console.WriteLine($"commitsitem: => {item.Commit.NodeId}");
+
+            //}
+
+            //var repo = "DemoApp";
+            //var ghClient = new GitHubClient(new ProductHeaderValue("DemoApp"));
+            //string sha1 = await ghClient.Repository.Commit.GetSha1(owner, repo, repo);
+
+                //var commit = await ghClient.Repository.Commit.Get(owner, repo, "c8108fad4ec7abac9f73bea2b4aa98cb2c9be343");
+                //Console.WriteLine($"commit.Filescommit.Files===>>>{commit.Files}");
+            }
+        //private static async Task Main2E()
+        //{
+
+        //    var repo1 = "DemoApp";
+
+        //    //Get branch info
+        //    GitHubClient client = new GitHubClient(new ProductHeaderValue("DemoApp"));
+        //    Repository repo = await client.Repository.Get(owner, repo1);
+        //    Branch branch = await client.Repository.Branch.Get(repo.Id, repo.DefaultBranch);
+        //    string sha1 = await client.Repository.Commit.GetSha1(repo.Id, branch.Commit.Sha);
+
+        //    var commit = await client.Repository.Commit.Get(owner, repo1, sha1);
+
+        //    Console.WriteLine($"commit.Filescommit.Files===>>>{commit.Files}");
+        //    //Print info
+        //    Console.WriteLine("Repository: " + repo.FullName);
+        //    Console.WriteLine("Branch: " + branch.Name);
+        //    Console.WriteLine("Branch SHA: " + branch.Commit.Sha);
+        //    Console.WriteLine("Branch SHA1: " + sha1);
+
+        //    //Keep info on screen
+        //    Console.WriteLine("\nPress enter to quit...");
+        //    Console.ReadLine();
+        //}
         private static void ReadXmlFile(string path)
         {
             string xmlFilePath = ConfigurationManager.AppSettings["xmlFilePath"];
@@ -90,7 +169,6 @@ namespace WatcherProject1
             XmlModel.testrun resFromXml = ser.Deserialize<XmlModel.testrun>(xmlInputData);
             xmlOutputData = ser.Serialize<XmlModel.testrun>(resFromXml);
 
-            Console.WriteLine(xmlOutputData);
             if (resFromXml.failed == 0)
             {
                 uplodaToGithub(path);
@@ -100,6 +178,7 @@ namespace WatcherProject1
                 Console.WriteLine("One or more of the tests do not pass");
             }
         }
+
         private static  void FileSystemWatcher_Changed(object sender, FileSystemEventArgs e)
         {
 
@@ -116,7 +195,10 @@ namespace WatcherProject1
             var processRnnar = Process.Start(nunitConsole, nunitDLL);
 
             processRnnar.WaitForExit();
+            //_ = CanGetFilesInCommit();
 
+            // _ = Main2E();
+            ComparisonBetween();
             ReadXmlFile(e.Name);
 
             Console.ReadLine();
