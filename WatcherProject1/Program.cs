8efb049077f37986433fe90f02cf0c6386aa379b
Moshe Yaso <yasomoshe@gmail.com>
2022-06-20 22:13:05 +0300

    commit

--- WatcherProject1/Program.cs
+++ WatcherProject1/Program.cs
@@ -18,33 +18,36 @@ namespace WatcherProject1
         static void Main(string[] args)
 
         {
-            WatchFile(watcher);
-            Console.ReadLine();
-
-            //  var pathDemoApp = ConfigurationManager.AppSettings["pathDemoApp"].Split(',');
 
 
-            //for (int i = 0; i < pathDemoApp.Length; i++)
-            //{
-            //    if (Directory.Exists(pathDemoApp[i]))
-            //    {
-            //        MonitorDirectory(pathDemoApp[i]);
-            //        break;
-            //    }
-            //}
+            var pathDemoApp = ConfigurationManager.AppSettings["pathDemoApp"].Split(',');
+
+
+            for (int i = 0; i < pathDemoApp.Length; i++)
+            {
+                if (Directory.Exists(pathDemoApp[i]))
+                {
+                    MonitorDirectory(pathDemoApp[i]);
+                    break;
+                }
+            }
 
 
         }
-        // set up the FileSystemWatcher object
-        private static void WatchFile(FileSystemWatcher watcher)
-        {
-            var pathDemoApp = @"C:\Users\User\Desktop\Project\DemoApp\DemoApp";
-            // ***** Change this as required
-            watcher.Path = pathDemoApp;
 
-            // For this example, I only care about when new files are
-            // created
-            watcher.NotifyFilter = NotifyFilters.Attributes
+
+
+
+
+        private static void MonitorDirectory(string path)
+
+        {
+
+            FileSystemWatcher fileSystemWatcher = new FileSystemWatcher(path);
+
+            fileSystemWatcher.Path = path;
+
+            fileSystemWatcher.NotifyFilter = NotifyFilters.Attributes
                                 | NotifyFilters.CreationTime
                                 | NotifyFilters.DirectoryName
                                 | NotifyFilters.FileName
@@ -53,93 +56,27 @@ namespace WatcherProject1
                                 | NotifyFilters.Security
                                 | NotifyFilters.Size;
 
-            // Add event handlers: 1 for the event raised when a file
-            // is created, and 1 for when it detects an error.
-            watcher.Changed += new FileSystemEventHandler(FileSystemWatcher_Changed);
-            watcher.Created += new FileSystemEventHandler(FileSystemWatcher_Created);
-            watcher.Deleted += new FileSystemEventHandler(FileSystemWatcher_Deleted);
-        //    watcher.Renamed += new FileSystemEventHandler(FileSystemWatcher_Renamed);
-           
-            
-            
-            watcher.Error += new ErrorEventHandler(WatcherError);
-           //    watcher.Filter = "*.cs";
+            fileSystemWatcher.Created += FileSystemWatcher_Created;
+            fileSystemWatcher.Changed += FileSystemWatcher_Changed;
+            fileSystemWatcher.Renamed += FileSystemWatcher_Renamed;
 
-            // Begin watching.
-            watcher.EnableRaisingEvents = true;
-        }
-        // Define the found event handler.
-        private static void NewFile(object source, FileSystemEventArgs e)
-        {
-            Console.WriteLine("A file has been found!");
-           // File.Delete(e.FullPath);
-        }
+            fileSystemWatcher.Deleted += FileSystemWatcher_Deleted;
 
-        // The error event handler
-        private static void WatcherError(object source, ErrorEventArgs e)
-        {
-            Exception watchException = e.GetException();
-            Console.WriteLine($"A FileSystemWatcher error has occurred: {watchException.Message}");
-            // We need to create new version of the object because the
-            // old one is now corrupted
-            watcher = new FileSystemWatcher();
-            while (!watcher.EnableRaisingEvents)
-            {
-                try
-                {
-                    // This will throw an error at the
-                    // watcher.NotifyFilter line if it can't get the path.
-                    WatchFile(watcher);
-                    Console.WriteLine("I'm Back!!");
-                }
-                catch
-                {
-                    // Sleep for a bit; otherwise, it takes a bit of
-                    // processor time
-                    System.Threading.Thread.Sleep(5000);
-                }
-            }
+            fileSystemWatcher.EnableRaisingEvents = true;
+            fileSystemWatcher.Filter = "*.cs";
+            fileSystemWatcher.IncludeSubdirectories = true;
+
+
+            Console.WriteLine("Press enter to exit.");
+            Console.ReadLine();
 
         }
 
-            //private static void MonitorDirectory(string path)
-
-            //{
-
-            //    FileSystemWatcher fileSystemWatcher = new FileSystemWatcher(path);
-
-            //    fileSystemWatcher.Path = path;
-
-            //    fileSystemWatcher.NotifyFilter = NotifyFilters.Attributes
-            //                        | NotifyFilters.CreationTime
-            //                        | NotifyFilters.DirectoryName
-            //                        | NotifyFilters.FileName
-            //                        | NotifyFilters.LastAccess
-            //                        | NotifyFilters.LastWrite
-            //                        | NotifyFilters.Security
-            //                        | NotifyFilters.Size;
-
-            //    fileSystemWatcher.Created += FileSystemWatcher_Created;
-            //    fileSystemWatcher.Changed += FileSystemWatcher_Changed;
-            //    fileSystemWatcher.Renamed += FileSystemWatcher_Renamed;
-
-            //    fileSystemWatcher.Deleted += FileSystemWatcher_Deleted;
-
-            //    fileSystemWatcher.EnableRaisingEvents = true;
-            //    fileSystemWatcher.Filter = "*.cs";
-            //    fileSystemWatcher.IncludeSubdirectories = true;
-
-
-            //    Console.WriteLine("Press enter to exit.");
-            //    Console.ReadLine();
-
-            //}
-
-            private static async void UplodaToGithub(string path)
+        private static async void UplodaToGithub(string path)
         {
             var ghClient = new GitHubClient(new ProductHeaderValue("DemoApp"));
 
-            ghClient.Credentials = new Credentials("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");
+            ghClient.Credentials = new Credentials("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");
             string owner = "mosmo46";
             var repo = "DemoApp";
             var master = "master";
@@ -250,15 +187,10 @@ namespace WatcherProject1
 
         private static void FileSystemWatcher_Changed(object sender, FileSystemEventArgs e)
         {
-
             MsBuild();
-
             RunTests();
-
-            ReadXmlFile(e.FullPath);
+            ReadXmlFile(e.Name);
             Console.WriteLine("File Changed: {0}", e.Name);
-            Console.WriteLine($"DateTime.Now=>{DateTime.Now}");
-           
         }
 
         private static void FileSystemWatcher_Created(object sender, FileSystemEventArgs e)
